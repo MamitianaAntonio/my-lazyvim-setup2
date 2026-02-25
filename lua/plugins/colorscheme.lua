bee7fea9fc6fcbf4cb2053a58c29cdb39fa38979
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-02-25 22:34:40 +0300

    feat(onedark) : implement new style usign onedark colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "dark",
+      transparent = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
